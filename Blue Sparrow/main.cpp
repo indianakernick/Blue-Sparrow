19dd457ed39fe3a1123661ff067ad953b98ce841
Kerndog73 <kerndog73@gmail.com>
2020-02-06 15:39:39 +1030

    Initial Commit

---
+++ Blue Sparrow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blue Sparrow
+//
+//  Created by Indi Kernick on 6/2/20.
+//  Copyright © 2020 Indiana Kernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
