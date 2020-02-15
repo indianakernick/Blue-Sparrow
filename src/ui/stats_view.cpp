9dd81cbdb79fc34fc4338e2437fff6c6df43efdf
Kerndog73 <kerndog73@gmail.com>
2020-02-16 09:55:41 +1030

    Show some more numbers

--- src/ui/stats_view.cpp
+++ src/ui/stats_view.cpp
@@ -31,8 +31,10 @@ bool StatsView::event(const SDL_Event &) {
 }
 
 void StatsView::render(SDL_Renderer *ren) {
-  auto view = reg.view<Hull, HullParams, CameraFocus>();
-  view.less([&](auto hull, auto params) {
+  auto view = reg.view<Hull, HullParams, Coins, MissileAmmo, CameraFocus>();
+  view.less([&](auto hull, auto params, auto coins, auto ammo) {
     FC_Draw(font.get(), ren, 0.0f, 0.0f, "Hull: %d/%d", hull.h, params.durability);
+    FC_Draw(font.get(), ren, 0.0f, 18.0f, "Coins: %d", coins.c);
+    FC_Draw(font.get(), ren, 0.0f, 36.0f, "Missiles: %d", ammo.n);
   });
 }
